417fa112ebd41b40f145904a13b327c145ee2de9
sjoys <s4317059@lsbu.ac.uk>
2024-12-02 15:44:08 +0000

    Add files via upload

---
+++ webapp/app2.py
@@ -0,0 +1,19 @@
+Python 3.9.16 (main, Mar  8 2023, 10:39:24) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> ​
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Joya<br> 431709<br> computer science'
+
+
+@app.route('/cakes')
+def cakes():
+    return 'yummy cakes'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+    
